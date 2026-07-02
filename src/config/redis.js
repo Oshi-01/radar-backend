70fc17fe7b6817126bf62c737c4fb82c0380ea8f
Oshi-01 <mozahidul@hubxpert.com>
2026-07-02 21:27:13 +0600

    preadis 0.1

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,15 @@
 const Redis = require("ioredis");
 
 const redisConfig = {
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
     maxRetriesPerRequest: null,
 };
 
-const connection = new Redis(redisConfig);
+const connection = process.env.REDIS_URL 
+    ? new Redis(process.env.REDIS_URL, redisConfig)
+    : new Redis({
+        ...redisConfig,
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: process.env.REDIS_PORT || 6379,
+    });
 
 module.exports = connection;
